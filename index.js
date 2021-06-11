14fa550a3cb238243fd8f4d886352583ffd9a6d0
die-perez <pzandie@gmail.com>
2021-06-11 15:20:25 -0700

    user comments and user info rendering

--- index.js
+++ index.js
@@ -5,15 +5,21 @@ let ejsLayouts = require('express-ejs-layouts')
 let db = require('./models')
 let moment = require('moment')
 let rowdy = require('rowdy-logger')
+const { response } = require('express')
 let app = express()
 
 var rowdyResults = rowdy.begin(app)
 
 app.set('view engine', 'ejs')
-
 app.use(express.urlencoded({ extended: false }))
 app.use(ejsLayouts)
 app.use(express.static(__dirname + '/public/'))
+
+// middleware that allows us to access the 'moment' library in every EJS view
+app.use((req, res, next) => {
+    res.locals.moment = moment
+    next()
+  })
 
 // GET --> home & intro screen for Vamp
 app.get('/', (req,res) => {
@@ -34,7 +40,7 @@ app.get('/products/search', (req,res) => {
     axios.get(`http://makeup-api.herokuapp.com/api/v1/products.json?product_type=${type}&brand=${brand}`)
         .then((response) => {
             // render data to page
-            res.render('products/results', { product: response.data})
+            res.render('products/results', { product: response.data })
         })
         .catch(err => {console.log(err)})
 })
@@ -44,8 +50,15 @@ app.get('/product/:id', (req,res) => {
     // search api data for id
     axios.get(`http://makeup-api.herokuapp.com/api/v1/products/${req.params.id}.json`)
     .then((response) => {
-        // render product details to page
-        res.render('products/detail', {product: response.data})
+        db.review.findAll({
+            where: {productId: req.params.id},
+            include: [db.user]
+        })
+        .then((reviews) => {
+            // render product details and comments to page
+            res.render('products/detail', { product: response.data, reviews })
+        })
+        .catch(err => {console.log(err)})
     })
     .catch(err => {console.log(err)})
 })
@@ -76,15 +89,28 @@ app.post('/product/:id/review', (req,res) => {
             defaults: {
                 content: content
             }
+        }).then(([review, created]) => {
+            res.redirect(`/product/${productId}`)
         })
-    })
-    .then(([review, created]) => {
-        res.redirect(`/product/${productId}`)
+        .catch(err => {console.log(err)}) 
     })
     .catch(err => {console.log(err)}) 
 })
 
-
+// GET --> user info
+app.get('/users/:id', (req,res) => {
+    // find all reviews by specific user
+    db.user.findOne({
+        where: {id: req.params.id},
+        include: [db.review]
+    })
+    .then((user) => {
+        console.log(user)
+        // render all reviews by this user to page
+        res.render('users/show', { user: user })
+    })
+    .catch(err => {console.log(err)})
+})
 
 var server = app.listen(process.env.PORT || 3000, () => {
     rowdyResults.print()
